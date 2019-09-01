986343661ed62654c58cd032c49eebf42998d784
j-oshi <jtoshin@msn.com>
2019-09-01 21:40:31 +0100

    remove data function

--- src/components/paragraph/TwoColText.vue
+++ src/components/paragraph/TwoColText.vue
@@ -24,21 +24,5 @@
 <script>
   export default {
     props: ['twoCols'],  
-    data: () => ({
-      items: [
-        {
-          color: "#1F7087",
-          src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-          title: "Supermodel",
-          artist: "Foster the People"
-        },
-        {
-          color: "#952175",
-          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
-          title: "Halcyon Days",
-          artist: "Ellie Goulding"
-        }
-      ]
-    })
   };
 </script>
