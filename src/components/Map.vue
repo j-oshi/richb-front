62675c107f5dca2db48ec2c35baf8b631e4a6991
j-oshi <jtoshin@msn.com>
2019-09-02 22:41:41 +0100

    add googlemap

---
+++ src/components/Map.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <gmap-map :center="{ lat: coordinates.lat, lng: coordinates.lon }" :zoom="15" style="width:100%;  height: 400px;">
+      <gmap-marker
+        :position="{ lat: coordinates.lat, lng: coordinates.lon }"
+      ></gmap-marker>
+    </gmap-map>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GoogleMap",
+    props: ["coordinates"],
+  };
+</script>
