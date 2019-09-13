4c341b1a34cdd82629f8e6e235b494482e732ce7
j-oshi <jtoshin@msn.com>
2019-09-13 21:35:07 +0100

    add subscription form

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -1,8 +1,13 @@
 <template>
   <v-footer color="grey darken-4" padless>
     <v-row justify="center" no-gutters>
-      <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2">{{ link }}</v-btn>
-      <v-col class="grey darken-3 py-4 text-center white--text" cols="12">
+      <v-col class="grey darken-3 text-center py-4 white--text" cols="12">
+        <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2">{{ link }}</v-btn>
+      </v-col>
+      <v-col class="grey darken-4 py-4 white--text" cols="12" md="3">
+        <subscribe />
+      </v-col>
+      <v-col class="grey darken-4 py-4 text-center white--text" cols="12">
         {{ new Date().getFullYear() }} —
         <strong>Ri</strong>
       </v-col>
@@ -12,6 +17,9 @@
 <script>
 
 export default {
+  components: {
+    Subscribe: () => import("@/components/Subscribe"),
+  },
   data: () => ({
     links: ["Home", "About Us", "Team", "Services", "Blog", "Contact Us"]
   })
